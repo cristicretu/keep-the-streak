f2eb0c5d8ddc6e4ec1b8a6d6be73782da4504419
Cristian Crețu <crisemcr@gmail.com>
2022-03-30 14:16:04 +0300

    [feat]: exit states

--- main.py
+++ main.py
@@ -1,10 +1,14 @@
 from bs4 import BeautifulSoup
 import requests
 import datetime
+import sys, os, errno
 
+# Config
 headers = {
     "User-Agent": 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) '
                   'Chrome/42.0.2311.135 Safari/537.36 Edge/12.246'}
+
+# Change this to your own url
 url = "https://www.github.com/cristicretu"
 
 # Get the page
@@ -22,4 +26,7 @@ stringday = today.strftime("%Y-%m-%d")
 # Get today's contributions
 result = soup.find_all("rect", {"data-date": stringday})[0]["data-count"]
 
-
+if result == "0":
+  sys.exit(os.EX_OK)
+else:
+  sys.exit(errno.ECANCELED) 
